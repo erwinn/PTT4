3aa6151db25aa0f925dc36bbe858bb9ba311717a
Boudewijns,Erwin E.A.C <e.boudewijns@student.fontys.nl>
2016-05-18 10:47:34 +0200

    New server and client structure

---
+++ CSA/TrainService/TrainService/ITrainService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TrainService
+{
+    [ServiceContract]
+    public interface ITrainService
+    {
+        
+    }
+
+    
+    [DataContract]
+    public class Server
+    {
+        
+    }
+}

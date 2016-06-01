d3c81504850f99c12f2d2176db9d807bbf1f2cae
Wout van den Dungen <woutvandendungen@outlook.com>
2016-06-01 10:34:10 +0200

    Merged Speedometer code with the Motortrein code

--- des/TreinMotor/motortrein/MotorControl.ino
+++ des/TreinMotor/motortrein/MotorControl.ino
@@ -1,32 +1,115 @@
-bool PrevValue = false;
-int MeasurementsDone = 0;
-long TimeOfLastMeasurement = 0;
-
-void RefreshRPM()
+//Calculates the speed of the motor
+//
+//int Accuracy,     the number wich defines how accurate the measurement is, the higher, the better
+void RefreshRPM(int Accuracy)
 {
-  int Val = analogRead(SpeedSensorPin);
-  bool isBlocked;
-  if (Val > 700) isBlocked = true;
-  else if (Val < 200)isBlocked = false;
-  else isBlocked = PrevValue;
-
-  if (isBlocked != PrevValue)
+  //if the wanted speed is 0, set RPM and Motor PWR to 0, else run the RPM measurement
+  if (Setpoint <= 0)
   {
-    PrevValue = isBlocked;
-    MeasurementsDone++;
-    if (MeasurementsDone >= 10)
-    {
-      double Time = millis() - TimeOfLastMeasurement;
-      TimeOfLastMeasurement = millis();
-      Time = 1 / (Time * 4);
-      Time *= 60000;
-      RPM = Time;
+    //set RPM and motorspeed to 0
+    analogWrite(MotorPWR, 0);
+    AddValue(RPM, ItemsInArray, 0);
+    CalculateAverage(RPM, ItemsInArray);
+  }
+  else
+  {
+    //Declaring Temporary Values
+    int MeasurementsDone = 0;
+    bool PrevValue = false;
+    long TimeOfLastMeasurement = millis();
 
-      MeasurementsDone = 0;
+    //Measure the time over the rotation of a quarter of the full circle
+    while (MeasurementsDone < (Accuracy * 2) && (millis() - TimeOfLastMeasurement) < (Accuracy * 100))
+    {
+      //Read if the IR sensor is Blocked Or Not
+      int Val = analogRead(SpeedSensorPin);
+      bool isBlocked = PrevValue;
+      if (Val > 700) isBlocked = true;
+      else if (Val < 200)isBlocked = false;
+
+      //if the state changes, Proceed with the next measurement
+      if (isBlocked != PrevValue)
+      {
+        PrevValue = isBlocked;
+        MeasurementsDone++;
+      }
+    }
+
+    //if the measurement timed out, set RPM to 0
+    if ((millis() - TimeOfLastMeasurement) > (Accuracy * 90))
+    {
+      AddValue(RPM, ItemsInArray, 0);
+    }
+
+    //if the measurement was succesfull, set the RPM to the actual RPM
+    else
+    {
+      double MeasuredTime = millis() - TimeOfLastMeasurement;
+      double TimeTensSeconds = MeasuredTime / 100;
+      AddValue(RPM, ItemsInArray, (30 * Accuracy) / TimeTensSeconds);
+    }
+
+    //Compute the new values for speed
+    CalculateAverage(RPM, ItemsInArray);
+    myPID.Compute();
+
+    //the motor needs a little help starting up, so if the motors starts turning
+    //briefly power the motor a bit harder then necessary
+    if (AverageRPM == 0)
+    {
+      analogWrite(MotorPWR, 80);
+      delay(50);
+    }
+
+    //Power the motor with the calculated Amount
+    analogWrite(MotorPWR, Output);
+  }
+}
+
+//Calculates the average nuber inside the array
+//
+//double Array[],   The array in wich all the data is stored
+//int Count,        The number of items inside the Array
+//
+//Returns The calculated average inside the array
+double CalculateAverage(double Array[], int Count)
+{
+  //declare local Variables
+  double total = 0;
+  int UsableValues = 0;
+
+  //Look inside every value inside the array
+  for (int i = 0; i < Count; i++)
+  {
+    //only select valid data, no timeout data
+    if (Array[i] > 0)
+    {
+      UsableValues++;
+      total += Array[i];
     }
   }
-  if (RPM != 0 && millis() - TimeOfLastMeasurement > 1000)
+
+  //if there is no valid data, set AverageRPM to 0
+  if (UsableValues <= 0) AverageRPM = 0;
+  else AverageRPM = total / UsableValues;
+
+  //return the AverageRPM
+  return AverageRPM;
+}
+
+//Moves all values inside the array forward by one, then inserts the new value in the last index
+//
+//double Array[],   The array in wich all the data is stored
+//int Count,        The number of items inside the Array
+//double value,     The value that needs to be placed inside the array
+void AddValue(double Array[], int Count, double value)
+{
+  //move every value forward by one
+  for (int i = 0; i < Count - 1; i++)
   {
-    RPM = 0;
+    Array[i] = Array[i + 1];
   }
+
+  //insert a new value at the last index of the array
+  Array[Count - 1] = value;
 }
--- des/TreinMotor/motortrein/motortrein.ino
+++ des/TreinMotor/motortrein/motortrein.ino
@@ -1,48 +1,77 @@
-//NIET AF
-
 #include <PID_v1.h>
 #include <SPI.h>
 #include <MCP2515.h>
 #include <SoftwareSerial.h>
+#include <serLCD.h>
 
 #define TREIN 0x7d3
-#define MotorPWR 3
-#define MotorDir 12
-#define SpeedSensorPin A5
 
-SoftwareSerial mySerial(5, 6);
-
-int delaynr = 5;
-String s = "";
 CANMSG msg;
 MCP2515 can;
 
+//Define used Pins
+#define MotorPWR 3
+#define MotorDir 12
+#define SpeedSensorPin A5
+#define LCDPin 6
+
+//Motor Constants
+#define MotorPowerMin 30      //the motor does not run under this value, it will stop
+#define MotorPowerMax 255     //the motor will show strange behaviour if this number is exceeded
+
+//The Wanted Accuracy (>0). the higher the number, the longer the test runs, and the more accurate the result is
+//Default = 5, a full circle = 20
+#define SpeedSensorAccuracy 5
+
+//Smoothing of SpeedData (>0)
+#define ItemsInArray 5
+
+//PID constants (0.1, 0.5, 0.0)
+#define Kp 0.1
+#define Ki 0.5
+#define Kd 0.0
+
+//PID Variables
 double Output;
+double AverageRPM;
+double Setpoint = 0;
+
+//Speed Sensor Data Array
+double RPM[ItemsInArray];
+
+//for measuring time in the code
 long TimePassed;
-double RPM;
-double Setpoint = 160;
 
-PID myPID(&RPM, &Output, &Setpoint, 0.6, 0.5, 0.1, DIRECT);
+//Setup PID Controller using above specified Variables
+PID myPID(&AverageRPM, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
+//Define SerialLCD Display
+serLCD lcd(LCDPin);
 
 void setup()
 {
-  mySerial.begin(9600);
-  mySerial.println("-----------------------------------------");
- mySerial.println("-----------------------------------------");
-  mySerial.println("-----------------------------------------");
-  mySerial.println("-----------------------------------------");
-  Setpoint=0;
+  //init Display
+  lcd.setBrightness(30);
+  lcd.clear();
+  lcd.print("Speedometer");
+
+  //Set PinMode
   pinMode(SpeedSensorPin, INPUT);
   pinMode(MotorPWR, OUTPUT);
   pinMode(MotorDir, OUTPUT);
+
+  //set the motor in the correct Direction
   digitalWrite(MotorDir, LOW);
 
-  myPID.SetMode(AUTOMATIC);
+  //Turn the PID Controller ON:
+  myPID.SetMode(HIGH);
 
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("Go...");
+  //Time between each PID calculation:
+  myPID.SetSampleTime(200);
+
+  //set limits to the output, in this case the motor
+  myPID.SetOutputLimits(MotorPowerMin, MotorPowerMax);
+
   SPI.setClockDivider(SPI_CLOCK_DIV8);
   if (can.initCAN(CAN_BAUD_100K) == 0)
   {
@@ -56,7 +85,6 @@ void setup()
   }
 }
 
-
 void loop()
 {
   //receiver
@@ -64,26 +92,36 @@ void loop()
   if (i && msg.adrsValue == TREIN)
   {
     int treinid = msg.data[1];
-    Setpoint=msg.data[0];
-    mySerial.println(String(Setpoint));
-    
-    //MotorControl
-    RefreshRPM();
-    myPID.Compute();
-    analogWrite(MotorPWR, Output);
+    Setpoint = msg.data[0];
   }
-}
-/*
-int extractintfromstring(String msg)
-{
-  String tempstring = "";
-  for (int i = 0; i < msg.length(); i++)
+  //MotorControl
+  RefreshRPM(SpeedSensorAccuracy);
+
+  //Display Sensor Data
+  if (millis() - TimePassed > 2000)
   {
-    if (isdigit(msg[i]) != 0)
-    {
-      tempstring += msg[i];
-    }
+    TimePassed = millis();
+    
+    //clear the data line:
+    lcd.selectLine(2);
+    lcd.clearLine(2);
+
+    //print the Current RPM, add padding 0 if neccesary:
+    lcd.print("RPM:          ");
+    if (AverageRPM < 100)  lcd.print('0');
+    if (AverageRPM < 10)  lcd.print('0');
+    lcd.print(AverageRPM);
+
+    //print the Current Output, add padding 0 if neccesary:
+    lcd.print("Output:       ");
+    if (Output < 100)  lcd.print('0');
+    if (Output < 10)  lcd.print('0');
+    lcd.print(Output);
+
+    //print the Current Setpoint, add padding 0 if neccesary:
+    lcd.print("Setpoint:     ");
+    if (Setpoint < 100)  lcd.print('0');
+    if (Setpoint < 10)  lcd.print('0');
+    lcd.print(Setpoint);
   }
-  return tempstring.toInt();
 }
-*/

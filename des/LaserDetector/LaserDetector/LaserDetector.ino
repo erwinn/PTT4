51c9c5efd34714712307662dc706bfb93832651e
Nick van den Heuvel <nickvdheuvel@hotmail.com>
2016-06-01 10:54:19 +0200

    Added Laser detection
    jojojojojo

--- des/LaserDetector/LaserDetector/LaserDetector.ino
+++ des/LaserDetector/LaserDetector/LaserDetector.ino
@@ -1,15 +1,37 @@
+int LDR1 = A0;
+int LDR2 = A1;
+int Value1 = 0;
+int Value2 = 0;
+bool Block1;
+bool Block2;
+
 void setup() {
   // put your setup code here, to run once:
-   pinMode(A0, INPUT);
-   pinMode(A1, INPUT);
+   pinMode(LDR1, INPUT);
+   pinMode(LDR2, INPUT);
    Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Sensor 1: ");
-  Serial.print(analogRead(A0));
-  Serial.print(", Sensor2: ");
-  Serial.println(analogRead(A1));
+ 
+  Value1 = analogRead(LDR1), Value2 = analogRead(LDR2);  
+  if (Value1 > 800)
+  {
+      Block1 = true;
+      //Serial.println("S1 is BLOCKED");
+  }
+  else{ 
+    Block1 = false;
+  }
+
+  if (Value2 > 800)
+  {
+    Block1 = true;
+    //Serial.println("S2 is BLOCKED");
+  }
+  else{
+    Block2 = false;
+  }
   delay(500);
 }
